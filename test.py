840f5a53567421b682fce184dfa515d83a8aa1ef
DinushaMS <dinusha89.usjp@gmail.com>
2024-03-09 13:59:51 -0500

    ignore test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-SMPs = []
-data_dir_path_1 = "D:\\Academic\\URI\\Lab\\experimental_data\\2022\\CARS"
-for directory in os.listdir(data_dir_path_1):
-    data_dir_path_2 = os.path.join(data_dir_path_1, directory)
-    for file in os.listdir(data_dir_path_2):
-        if "Notes" in file:
-            print(file)
